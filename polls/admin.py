ee1aa402cbac4913a770191330f1f9eb180f541e
Yaroslav <y.zhuvanov@riseapps.biz>
2021-12-11 11:59:11 +0200

    flake8

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Question, Choice
+from .models import Choice, Question
 
 
 class ChoiceInline(admin.TabularInline):
@@ -13,7 +13,7 @@ class QuestionAdmin(admin.ModelAdmin):
     list_filter = ['pub_date']
     search_fields = ['question_text']
     fieldsets = [
-        (None,               {'fields': ['question_text']}),
+        (None, {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
