0291e1585c51eb7e711462dd96a3446b0fc1d46e
Yaroslav <qwaswexexortinvoke@gmail.com>
2022-10-15 12:28:35 +0300

    travisci example 2

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,10 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-
-from .models import Choice, Question
 from django.utils import timezone
 from django.views import generic
+
+from .models import Choice, Question
 
 
 class IndexView(generic.ListView):
